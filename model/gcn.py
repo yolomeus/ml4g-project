8802d0995eb2594787d30a62d57ff1746bfabccd
Fabian <fab.beringer@gmail.com>
2022-01-12 17:56:37 +0100

    add fagcn implementation

--- model/gcn.py
+++ model/gcn.py
@@ -6,6 +6,7 @@ from torch_geometric.nn import GCNConv, FAConv
 class GCN(Module):
     """Simple 2-layer GCN.
     """
+
     def __init__(self, in_dim, h_dim, out_dim, dropout):
         super().__init__()
         self.conv1 = GCNConv(in_dim, h_dim)
@@ -19,3 +20,23 @@ class GCN(Module):
         x = self.conv2(x, edge_index)
 
         return x
+
+
+class FAGCN(Module):
+    def __init__(self, in_dim, h_dim, out_dim, eps, dropout, n_layers, activation: Module = ReLU()):
+        super().__init__()
+        self.lin_in = Linear(in_dim, h_dim)
+        self.layers = ModuleList([FAConv(h_dim, eps, dropout=dropout)
+                                  for _ in range(n_layers)])
+        self.lin_out = Linear(h_dim, out_dim)
+
+        self.act = activation
+
+    def forward(self, x, edge_index):
+        x = self.lin_in(x)
+        x_0 = self.act(x)
+        x = x_0
+        for layer in self.layers:
+            x = layer(x, x_0, edge_index)
+
+        return self.lin_out(x)
